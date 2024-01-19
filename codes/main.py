03238f0c2ec6d79944999060a5961d2de42f2f04
Yukayo <33035372+Yukayo@users.noreply.github.com>
2024-01-19 16:56:19 +0900

    Update main.py

--- codes/main.py
+++ codes/main.py
@@ -15,9 +15,9 @@ from json import encoder
 
 encoder.FLOAT_REPR = lambda o: format(o, '.3f')
 
-from train import run_simple, RnnParameterData, generate_input_history, markov, \
-    generate_input_long_history, generate_input_long_history2
-from model import TrajPreSimple, TrajPreAttnAvgLongUser, TrajPreLocalAttnLong
+from train import run_simple, RnnParameterData, generate_input_history, \
+    generate_input_long_history2
+from model import TrajPreSimple
 
 
 def run(args):
@@ -25,35 +25,22 @@ def run(args):
                                   voc_emb_size=args.voc_emb_size, tim_emb_size=args.tim_emb_size,
                                   hidden_size=args.hidden_size, dropout_p=args.dropout_p,
                                   data_name=args.data_name, lr=args.learning_rate,
-                                  lr_step=args.lr_step, lr_decay=args.lr_decay, L2=args.L2, rnn_type=args.rnn_type,
-                                  optim=args.optim, attn_type=args.attn_type,
-                                  clip=args.clip, epoch_max=args.epoch_max, history_mode=args.history_mode,
+                                  lr_step=args.lr_step, lr_decay=args.lr_decay, L2=args.L2,
+                                  optim=args.optim, clip=args.clip, epoch_max=args.epoch_max, 
                                   model_mode=args.model_mode, data_path=args.data_path, save_path=args.save_path)
     argv = {'loc_emb_size': args.loc_emb_size, 'uid_emb_size': args.uid_emb_size, 'voc_emb_size': args.voc_emb_size,
             'tim_emb_size': args.tim_emb_size, 'hidden_size': args.hidden_size,
             'dropout_p': args.dropout_p, 'data_name': args.data_name, 'learning_rate': args.learning_rate,
             'lr_step': args.lr_step, 'lr_decay': args.lr_decay, 'L2': args.L2, 'act_type': 'selu',
-            'optim': args.optim, 'attn_type': args.attn_type, 'clip': args.clip, 'rnn_type': args.rnn_type,
-            'epoch_max': args.epoch_max, 'history_mode': args.history_mode, 'model_mode': args.model_mode}
+            'optim': args.optim, 'clip': args.clip, 'epoch_max': args.epoch_max}
     print('*' * 15 + 'start training' + '*' * 15)
-    print('model_mode:{} history_mode:{} users:{}'.format(
-        parameters.model_mode, parameters.history_mode, parameters.uid_size))
+    print('users:{}'.format(parameters.uid_size))
 
-    if parameters.model_mode in ['simple', 'simple_long']:
-        model = TrajPreSimple(parameters=parameters).cuda()
-    elif parameters.model_mode == 'attn_avg_long_user':
-        model = TrajPreAttnAvgLongUser(parameters=parameters).cuda()
-    elif parameters.model_mode == 'attn_local_long':
-        model = TrajPreLocalAttnLong(parameters=parameters).cuda()
+    model = TrajPreSimple(parameters=parameters).cuda()
     if args.pretrain == 1:
-        model.load_state_dict(torch.load("../pretrain/" + args.model_mode + "/res.m"))
+        model.load_state_dict(torch.load("./pretrain/" + args.model_mode + "/res.m"))
 
-    if 'max' in parameters.model_mode:
-        parameters.history_mode = 'max'
-    elif 'avg' in parameters.model_mode:
-        parameters.history_mode = 'avg'
-    else:
-        parameters.history_mode = 'whole'
+
 
     criterion = nn.NLLLoss().cuda()
     optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=parameters.lr,
@@ -65,30 +52,12 @@ def run(args):
     metrics = {'train_loss': [], 'valid_loss': [], 'accuracy': [], 'valid_acc': {}}
 
     candidate = parameters.data_neural.keys()
-    avg_acc_markov, users_acc_markov = markov(parameters, candidate)
-    metrics['markov_acc'] = users_acc_markov
 
-    if 'long' in parameters.model_mode:
-        long_history = True
-    else:
-        long_history = False
 
-    if long_history is False:
-        data_train, train_idx = generate_input_history(parameters.data_neural, 'train', mode2=parameters.history_mode,
-                                                       candidate=candidate)
-        data_test, test_idx = generate_input_history(parameters.data_neural, 'test', mode2=parameters.history_mode,
-                                                     candidate=candidate)
-    elif long_history is True:
-        if parameters.model_mode == 'simple_long':
-            data_train, train_idx = generate_input_long_history2(parameters.data_neural, 'train', candidate=candidate)
-            data_test, test_idx = generate_input_long_history2(parameters.data_neural, 'test', candidate=candidate)
-        else:
-            data_train, train_idx = generate_input_long_history(parameters.data_neural, 'train', candidate=candidate)
-            data_test, test_idx = generate_input_long_history(parameters.data_neural, 'test', candidate=candidate)
+    data_train, train_idx = generate_input_history(parameters.data_neural, 'train', candidate=candidate)
+    data_test, test_idx = generate_input_history(parameters.data_neural, 'test', candidate=candidate)
 
-    print('users:{} markov:{} train:{} test:{}'.format(len(candidate), avg_acc_markov,
-                                                       len([y for x in train_idx for y in train_idx[x]]),
-                                                       len([y for x in test_idx for y in test_idx[x]])))
+
     SAVE_PATH = args.save_path
     tmp_path = 'checkpoint/'
     if os.path.exists(SAVE_PATH + tmp_path)==False:
@@ -149,7 +118,7 @@ def run(args):
 
 
 def load_pretrained_model(config):
-    res = json.load(open("../pretrain/" + config.model_mode + "/res.txt"))
+    res = json.load(open("./pretrain/" + config.model_mode + "/res.txt"))
     args = Settings(config, res["args"])
     return args
 
@@ -165,10 +134,7 @@ class Settings(object):
         self.lr_decay = res["lr_decay"]
         self.clip = res["clip"]
         self.dropout_p = res["dropout_p"]
-        self.rnn_type = res["rnn_type"]
-        self.attn_type = res["attn_type"]
         self.L2 = res["L2"]
-        self.history_mode = res["history_mode"]
         self.model_mode = res["model_mode"]
         self.optim = res["optim"]
         self.hidden_size = res["hidden_size"]
@@ -191,7 +157,7 @@ if __name__ == '__main__':
     parser.add_argument('--tim_emb_size', type=int, default=10, help="time embeddings size")
     parser.add_argument('--hidden_size', type=int, default=500)
     parser.add_argument('--dropout_p', type=float, default=0.3)
-    parser.add_argument('--data_name', type=str, default='porto')
+    parser.add_argument('--data_name', type=str, default='foursquare', choices=['foursquare', 'porto'])
     parser.add_argument('--learning_rate', type=float, default=5 * 1e-4)
     parser.add_argument('--lr_step', type=int, default=2)
     parser.add_argument('--lr_decay', type=float, default=0.1)
@@ -199,13 +165,9 @@ if __name__ == '__main__':
     parser.add_argument('--L2', type=float, default=1 * 1e-5, help=" weight decay (L2 penalty)")
     parser.add_argument('--clip', type=float, default=5.0)
     parser.add_argument('--epoch_max', type=int, default=20)
-    parser.add_argument('--history_mode', type=str, default='avg', choices=['max', 'avg', 'whole'])
-    parser.add_argument('--rnn_type', type=str, default='LSTM', choices=['LSTM', 'GRU', 'RNN'])
-    parser.add_argument('--attn_type', type=str, default='dot', choices=['general', 'concat', 'dot'])
-    parser.add_argument('--data_path', type=str, default='../data/')
-    parser.add_argument('--save_path', type=str, default='../results/')
-    parser.add_argument('--model_mode', type=str, default='simple',
-                        choices=['simple', 'simple_long', 'attn_avg_long_user', 'attn_local_long'])
+    parser.add_argument('--data_path', type=str, default='./data/')
+    parser.add_argument('--save_path', type=str, default='./results/')
+    parser.add_argument('--model_mode', type=str, default='simple')
     parser.add_argument('--pretrain', type=int, default=0)
     args = parser.parse_args()
     if args.pretrain == 1:
