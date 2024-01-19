7263b3ecd1485909074fc15113258dfa7b9878be
Yukayo <33035372+Yukayo@users.noreply.github.com>
2024-01-19 16:56:02 +0900

    Update train.py

--- codes/train.py
+++ codes/train.py
@@ -13,12 +13,14 @@ from collections import deque, Counter
 class RnnParameterData(object):
     def __init__(self, loc_emb_size=500, uid_emb_size=40, voc_emb_size=50, tim_emb_size=10, hidden_size=500,
                  lr=1e-3, lr_step=3, lr_decay=0.1, dropout_p=0.5, L2=1e-5, clip=5.0, optim='Adam',
-                 history_mode='avg', attn_type='dot', epoch_max=30, rnn_type='LSTM', model_mode="simple",
-                 data_path='../data/', save_path='../results/', data_name='foursquare'):
+                  epoch_max=30, model_mode="simple",
+                 data_path='./data/', save_path='./results/', data_name='foursquare'):
         self.data_path = data_path
         self.save_path = save_path
         self.data_name = data_name
-        data = pk.load(open(self.data_path + self.data_name + '.pk', 'rb'))
+        with open(self.data_path + self.data_name + '.pk', 'rb') as file:
+            data = pk.load(file, encoding='latin1')
+        # data = pk.load(open(self.data_path + self.data_name + '.pk', encoding='utf-8'))
         self.vid_list = data['vid_list']
         self.uid_list = data['uid_list']
         self.data_neural = data['data_neural']
@@ -42,13 +44,10 @@ class RnnParameterData(object):
         self.L2 = L2
         self.clip = clip
 
-        self.attn_type = attn_type
-        self.rnn_type = rnn_type
-        self.history_mode = history_mode
         self.model_mode = model_mode
 
 
-def generate_input_history(data_neural, mode, mode2=None, candidate=None):
+def generate_input_history(data_neural, mode, candidate=None):
     data_train = {}
     train_idx = {}
     if candidate is None:
@@ -81,47 +80,25 @@ def generate_input_history(data_neural, mode, mode2=None, candidate=None):
             history = sorted(history, key=lambda x: x[1], reverse=False)
 
             # merge traces with same time stamp
-            if mode2 == 'max':
-                history_tmp = {}
-                for tr in history:
-                    if tr[1] not in history_tmp:
-                        history_tmp[tr[1]] = [tr[0]]
-                    else:
-                        history_tmp[tr[1]].append(tr[0])
-                history_filter = []
-                for t in history_tmp:
-                    if len(history_tmp[t]) == 1:
-                        history_filter.append((history_tmp[t][0], t))
-                    else:
-                        tmp = Counter(history_tmp[t]).most_common()
-                        if tmp[0][1] > 1:
-                            history_filter.append((history_tmp[t][0], t))
-                        else:
-                            ti = np.random.randint(len(tmp))
-                            history_filter.append((tmp[ti][0], t))
-                history = history_filter
-                history = sorted(history, key=lambda x: x[1], reverse=False)
-            elif mode2 == 'avg':
-                history_tim = [t[1] for t in history]
-                history_count = [1]
-                last_t = history_tim[0]
-                count = 1
-                for t in history_tim[1:]:
-                    if t == last_t:
-                        count += 1
-                    else:
-                        history_count[-1] = count
-                        history_count.append(1)
-                        last_t = t
-                        count = 1
+            history_tim = [t[1] for t in history]
+            history_count = [1]
+            last_t = history_tim[0]
+            count = 1
+            for t in history_tim[1:]:
+                if t == last_t:
+                    count += 1
+                else:
+                    history_count[-1] = count
+                    history_count.append(1)
+                    last_t = t
+                    count = 1
             ################
 
             history_loc = np.reshape(np.array([s[0] for s in history]), (len(history), 1))
             history_tim = np.reshape(np.array([s[1] for s in history]), (len(history), 1))
             trace['history_loc'] = Variable(torch.LongTensor(history_loc))
             trace['history_tim'] = Variable(torch.LongTensor(history_tim))
-            if mode2 == 'avg':
-                trace['history_count'] = history_count
+            trace['history_count'] = history_count
 
             data_train[u][i] = trace
         train_idx[u] = train_id
@@ -157,61 +134,6 @@ def generate_input_long_history2(data_neural, mode, candidate=None):
             train_idx[u] = [0, i]
         else:
             train_idx[u] = [i]
-    return data_train, train_idx
-
-
-def generate_input_long_history(data_neural, mode, candidate=None):
-    data_train = {}
-    train_idx = {}
-    if candidate is None:
-        candidate = data_neural.keys()
-    for u in candidate:
-        sessions = data_neural[u]['sessions']
-        train_id = data_neural[u][mode]
-        data_train[u] = {}
-        for c, i in enumerate(train_id):
-            trace = {}
-            if mode == 'train' and c == 0:
-                continue
-            session = sessions[i]
-            target = np.array([s[0] for s in session[1:]])
-
-            history = []
-            if mode == 'test':
-                test_id = data_neural[u]['train']
-                for tt in test_id:
-                    history.extend([(s[0], s[1]) for s in sessions[tt]])
-            for j in range(c):
-                history.extend([(s[0], s[1]) for s in sessions[train_id[j]]])
-
-            history_tim = [t[1] for t in history]
-            history_count = [1]
-            last_t = history_tim[0]
-            count = 1
-            for t in history_tim[1:]:
-                if t == last_t:
-                    count += 1
-                else:
-                    history_count[-1] = count
-                    history_count.append(1)
-                    last_t = t
-                    count = 1
-
-            history_loc = np.reshape(np.array([s[0] for s in history]), (len(history), 1))
-            history_tim = np.reshape(np.array([s[1] for s in history]), (len(history), 1))
-            trace['history_loc'] = Variable(torch.LongTensor(history_loc))
-            trace['history_tim'] = Variable(torch.LongTensor(history_tim))
-            trace['history_count'] = history_count
-
-            loc_tim = history
-            loc_tim.extend([(s[0], s[1]) for s in session[:-1]])
-            loc_np = np.reshape(np.array([s[0] for s in loc_tim]), (len(loc_tim), 1))
-            tim_np = np.reshape(np.array([s[1] for s in loc_tim]), (len(loc_tim), 1))
-            trace['loc'] = Variable(torch.LongTensor(loc_np))
-            trace['tim'] = Variable(torch.LongTensor(tim_np))
-            trace['target'] = Variable(torch.LongTensor(target))
-            data_train[u][i] = trace
-        train_idx[u] = train_id
     return data_train, train_idx
 
 
@@ -306,19 +228,7 @@ def run_simple(data, run_idx, mode, lr, clip, model, optimizer, criterion, mode2
         target = data[u][i]['target'].cuda()
         uid = Variable(torch.LongTensor([u])).cuda()
 
-        if 'attn' in mode2:
-            history_loc = data[u][i]['history_loc'].cuda()
-            history_tim = data[u][i]['history_tim'].cuda()
-
-        if mode2 in ['simple', 'simple_long']:
-            scores = model(loc, tim)
-        elif mode2 == 'attn_avg_long_user':
-            history_count = data[u][i]['history_count']
-            target_len = target.data.size()[0]
-            scores = model(loc, tim, history_loc, history_tim, history_count, uid, target_len)
-        elif mode2 == 'attn_local_long':
-            target_len = target.data.size()[0]
-            scores = model(loc, tim, target_len)
+        scores = model(loc, tim)
 
         if scores.data.size()[0] > target.data.size()[0]:
             scores = scores[-target.data.size()[0]:]
@@ -351,69 +261,3 @@ def run_simple(data, run_idx, mode, lr, clip, model, optimizer, criterion, mode2
             users_rnn_acc[u] = tmp_acc.tolist()[0]
         avg_acc = np.mean([users_rnn_acc[x] for x in users_rnn_acc])
         return avg_loss, avg_acc, users_rnn_acc
-
-
-def markov(parameters, candidate):
-    validation = {}
-    for u in candidate:
-        traces = parameters.data_neural[u]['sessions']
-        train_id = parameters.data_neural[u]['train']
-        test_id = parameters.data_neural[u]['test']
-        trace_train = []
-        for tr in train_id:
-            trace_train.append([t[0] for t in traces[tr]])
-        locations_train = []
-        for t in trace_train:
-            locations_train.extend(t)
-        trace_test = []
-        for tr in test_id:
-            trace_test.append([t[0] for t in traces[tr]])
-        locations_test = []
-        for t in trace_test:
-            locations_test.extend(t)
-        validation[u] = [locations_train, locations_test]
-    acc = 0
-    count = 0
-    user_acc = {}
-    for u in validation.keys():
-        topk = list(set(validation[u][0]))
-        transfer = np.zeros((len(topk), len(topk)))
-
-        # train
-        sessions = parameters.data_neural[u]['sessions']
-        train_id = parameters.data_neural[u]['train']
-        for i in train_id:
-            for j, s in enumerate(sessions[i][:-1]):
-                loc = s[0]
-                target = sessions[i][j + 1][0]
-                if loc in topk and target in topk:
-                    r = topk.index(loc)
-                    c = topk.index(target)
-                    transfer[r, c] += 1
-        for i in range(len(topk)):
-            tmp_sum = np.sum(transfer[i, :])
-            if tmp_sum > 0:
-                transfer[i, :] = transfer[i, :] / tmp_sum
-
-        # validation
-        user_count = 0
-        user_acc[u] = 0
-        test_id = parameters.data_neural[u]['test']
-        for i in test_id:
-            for j, s in enumerate(sessions[i][:-1]):
-                loc = s[0]
-                target = sessions[i][j + 1][0]
-                count += 1
-                user_count += 1
-                if loc in topk:
-                    pred = np.argmax(transfer[topk.index(loc), :])
-                    if pred >= len(topk) - 1:
-                        pred = np.random.randint(len(topk))
-
-                    pred2 = topk[pred]
-                    if pred2 == target:
-                        acc += 1
-                        user_acc[u] += 1
-        user_acc[u] = user_acc[u] / user_count
-    avg_acc = np.mean([user_acc[u] for u in user_acc])
-    return avg_acc, user_acc
